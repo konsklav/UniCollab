9870529e4a69f64926466ea3a01c014202b642e6
George Nikolaidis <cptgiorgos03@gmail.com>
2024-12-28 18:47:04 +0200

    Remove docs from Program.cs

--- apps/api/Saas.Api/Saas.Api/Program.cs
+++ apps/api/Saas.Api/Saas.Api/Program.cs
@@ -1,7 +1,5 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
@@ -11,5 +9,5 @@ app.MapOpenApi();
 app.UseHttpsRedirection();
 
 app.MapGet("/test", () => $"Testing! The time is {DateTime.UtcNow}");
-//
+
 app.Run();
