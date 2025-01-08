44a07fff84fbff4f73c8e2b36f4e65ad8954ae06
George Nikolaidis <cptgiorgos03@gmail.com>
2025-01-08 20:42:38 +0200

    Add page components

--- apps/website/src/App.tsx
+++ apps/website/src/App.tsx
@@ -1,13 +1,8 @@
 import './fun.css'
-import Test from './Test'
 
 export default function App() {
   return (
     <>
-    <div className='container'>
-      <h1 className='spin'>SaaS Project, Oh Shiiiit!</h1>
-      <Test/>
-      </div>
     </>
   )
 }
