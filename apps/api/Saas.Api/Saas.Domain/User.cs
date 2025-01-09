d8d25e6c632515dd649c22eb7ad23fd4425b30e9
George Nikolaidis <cptgiorgos03@gmail.com>
2025-01-09 16:14:04 +0200

    Add AddFriend() and RemoveFriend() behaviors

--- apps/api/Saas.Api/Saas.Domain/User.cs
+++ apps/api/Saas.Api/Saas.Domain/User.cs
@@ -1,9 +1,35 @@
+using Ardalis.Result;
+
 namespace Saas.Domain;
 
 public class User(Guid id, string username, string password, List<User> friends)
 {
+    private readonly List<User> _friends = friends;
+    
     public Guid Id { get; } = id;
     public string Username { get; } = username;
     public string Password { get; } = password;
-    public List<User> Friends { get; } = friends;
+
+    public IReadOnlyList<User> Friends => _friends;
+
+    public Result AddFriend(User user)
+    {
+        if (user == this)
+            return Result.Invalid();
+
+        if (Friends.Contains(user))
+            return Result.Conflict();
+
+        _friends.Add(user);
+        return Result.Success();
+    }
+
+    public Result RemoveFriend(User user)
+    {
+        if (!Friends.Contains(user))
+            return Result.NotFound();
+                        
+        _friends.Remove(user);
+        return Result.Success();
+    }
 }
