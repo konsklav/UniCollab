4595559a2f63b2b5346f8be4910055c2c52d5cc9
Konstantinos Sklavenitis <konstantinos.sklaveniths@gmail.com>
2025-01-10 17:06:52 +0200

    Implemented behaviours for ChatRooms (AddParticipant, RemoveParticipant, AddMessage, with Tests!)

---
+++ apps/api/Saas.Api/Saas.Domain.Tests/ChatRoomTests.cs
@@ -0,0 +1,121 @@
+using Ardalis.Result;
+using FluentAssertions;
+
+namespace Saas.Domain.Tests;
+
+public class ChatRoomTests
+{
+    [Fact]
+    public void AddParticipant_Should_AddUserToTheParticipantsList()
+    {
+        // Arrange
+        var user1 = new User(Guid.NewGuid(), "Test", "password", []);
+        var user2 = new User(Guid.NewGuid(), "Test 2", "password", [user1]);
+
+        var chatRoom = new ChatRoom(Guid.NewGuid(), "test", "test", [user2], []);   // I just added user2 in there as a participant
+        var initialparticipantsCount = chatRoom.Participants.Count;
+        
+        // Act
+        var result = chatRoom.AddParticipant(user1);
+
+        // Assert
+        result.IsSuccess.Should().BeTrue();
+        
+        chatRoom.Participants.Should().Contain(user1);
+        chatRoom.Participants.Count.Should().Be(initialparticipantsCount + 1);
+    }
+
+    [Fact]
+    public void AddParticipant_Should_ReturnConflict_WhenUserIsAlreadyAParticipant()
+    {
+        // Arrange
+        var user1 = new User(Guid.NewGuid(), "Test", "password", []);
+        var user2 = new User(Guid.NewGuid(), "Test 2", "password", [user1]);
+
+        var chatRoom = new ChatRoom(Guid.NewGuid(), "test", "test", [user1, user2], []);
+        
+        // Act
+        var result = chatRoom.AddParticipant(user1);
+
+        // Assert
+        result.IsSuccess.Should().BeFalse();
+        result.IsConflict().Should().BeTrue();
+    }
+
+    [Fact]
+    public void RemoveParticipant_Should_RemoveParticipantFromTheParticipantsList()
+    {
+        // Arrange
+        var user1 = new User(Guid.NewGuid(), "Test", "password", []);
+
+        var chatRoom = new ChatRoom(Guid.NewGuid(), "test", "test", [user1], []);
+        
+        var initialparticipantsCount = chatRoom.Participants.Count;
+        
+        // Act
+        var result = chatRoom.RemoveParticipant(user1);
+
+        // Assert
+        result.IsSuccess.Should().BeTrue();
+        
+        chatRoom.Participants.Should().NotContain(user1);
+        chatRoom.Participants.Count.Should().Be(initialparticipantsCount - 1);
+    }
+
+    [Fact]
+    public void RemoveParticipant_Should_ReturnNotFound_WhenUserIsNotInParticipantsList()
+    {
+        // Arrange
+        var user1 = new User(Guid.NewGuid(), "Test", "password", []);
+        var user2 = new User(Guid.NewGuid(), "Test 2", "password", [user1]);
+
+        var chatRoom = new ChatRoom(Guid.NewGuid(), "test", "test", [user2], []);   // I just added user2 in there as a participant
+        
+        // Act
+        var result = chatRoom.RemoveParticipant(user1);
+
+        // Assert
+        result.IsSuccess.Should().BeFalse();
+        result.IsNotFound().Should().BeTrue();
+    }
+
+    [Fact]
+    public void AddMessage_Should_AddMessageToTheMessagesList()
+    {
+        // Arrange
+        var user = new User(Guid.NewGuid(), "Test", "password", []);
+        
+        var message = new Message(Guid.NewGuid(), "Test", DateTime.Now, user);
+        
+        var chatRoom = new ChatRoom(Guid.NewGuid(), "test", "test", [user], []);
+        
+        var initialMessagesCount = chatRoom.Messages.Count;
+        
+        // Act
+        var result = chatRoom.AddMessage(message);
+        
+        // Assert
+        result.IsSuccess.Should().BeTrue();
+        
+        chatRoom.Messages.Should().Contain(message);
+        chatRoom.Messages.Count.Should().Be(initialMessagesCount + 1);
+    }
+
+    [Fact]
+    public void AddMessage_Should_ReturnConflict_WhenMessageIsAlreadyInMessagesList()
+    {
+        // Arrange
+        var user = new User(Guid.NewGuid(), "Test", "password", []);
+
+        var message = new Message(Guid.NewGuid(), "Test", DateTime.Now, user);
+        
+        var chatRoom = new ChatRoom(Guid.NewGuid(), "test", "test", [user], [message]);
+        
+        // Act
+        var result = chatRoom.AddMessage(message);
+
+        // Assert
+        result.IsSuccess.Should().BeFalse();
+        result.IsConflict().Should().BeTrue();
+    }
+}
--- apps/api/Saas.Api/Saas.Domain/ChatRoom.cs
+++ apps/api/Saas.Api/Saas.Domain/ChatRoom.cs
@@ -1,10 +1,42 @@
+using Ardalis.Result;
+
 namespace Saas.Domain;
 
 public class ChatRoom(Guid id, string name, string type, List<User> participants, List<Message> messages)
 {
+    private readonly List<User> _participants = participants;
+    private readonly List<Message> _messages = messages;
+    
     public Guid Id { get; } = id;
     public string Name { get; } = name;
     public string Type { get; } = type;
-    public List<User> Participants { get; } = participants;
-    public List<Message> Messages { get; } = messages;
+    public IReadOnlyList<User> Participants => _participants;
+    public List<Message> Messages => _messages;
+
+    public Result AddParticipant(User user)
+    {
+        if (Participants.Contains(user))
+            return Result.Conflict();
+        
+        _participants.Add(user);
+        return Result.Success();
+    }
+    
+    public Result RemoveParticipant(User user)
+    {
+        if (!Participants.Contains(user))
+            return Result.NotFound();
+        
+        _participants.Remove(user);
+        return Result.Success();
+    }
+    
+    public Result AddMessage(Message message)
+    {
+        if (Messages.Contains(message))
+            return Result.Conflict();
+        
+        _messages.Add(message);
+        return Result.Success();
+    }
 }
