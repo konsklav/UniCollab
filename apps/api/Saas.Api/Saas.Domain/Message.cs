6481258610eff4995236c1533c02c2892cb4908c
George Nikolaidis <cptgiorgos03@gmail.com>
2025-01-11 19:57:49 +0200

    Make Message.cs extend Entity

--- apps/api/Saas.Api/Saas.Domain/Message.cs
+++ apps/api/Saas.Api/Saas.Domain/Message.cs
@@ -1,6 +1,13 @@
+using Saas.Domain.Common;
+
 namespace Saas.Domain;
 
 /// <summary>
 /// Contains information about the message, when it was sent and who sent it. 
 /// </summary>
-public record Message(string Content, DateTime SentAt, User Sender);
+public class Message(string content, DateTime sentAt, User sender, Guid? id = null) : Entity(id)
+{
+    public string Content { get; private set; } = content;
+    public DateTime SentAt { get; private set; } = sentAt;
+    public User Sender { get; private set; } = sender;
+}
