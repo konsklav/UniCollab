d913eebaa8cf8858a606ceae10264e75f6837c98
George Nikolaidis <cptgiorgos03@gmail.com>
2025-01-15 00:41:13 +0200

    Added cool animated background in GuestLayout

--- apps/website/src/layout/GuestLayout.tsx
+++ apps/website/src/layout/GuestLayout.tsx
@@ -5,11 +5,16 @@ import styles from './GuestLayout.module.css'
 
 export default function GuestLayout() {
     return (
-        <div id={styles['auth']}>
-            <div id={styles['auth-logo']}>
-                <Logo color="dark"/>
+        <>
+            <div id={styles['backdrop']}></div>
+            <div id={styles['auth']}>
+                <div id={styles['auth-logo']}>
+                    <Logo color="dark"/>
+                </div>
+                <main id={styles['content']}>
+                    <Outlet/>
+                </main>
             </div>
-            <Outlet/>
-        </div>
+        </>
     )
 }
