86d637bd792e98e2f86a6f0ac51bb31aa083670f
George Nikolaidis <cptgiorgos03@gmail.com>
2025-01-14 18:27:16 +0200

    Added tests for LoginForm.

---
+++ apps/website/src/features/Login/LoginForm.test.tsx
@@ -0,0 +1,39 @@
+import { fireEvent, render, screen } from "@testing-library/react"
+import LoginForm from "./LoginForm"
+import { LoginCredentials } from "./Login.types"
+
+describe('LoginForm', () => {
+    test('Renders two input fields and a submit button', () => {
+        render(<LoginForm onLogin={jest.fn()}/>)
+        
+        const inputs = screen.getAllByRole('textbox')
+        expect(inputs).toHaveLength(2)
+
+        const buttons = screen.getAllByRole('button')
+        expect(buttons).toHaveLength(1)
+
+    })
+
+    test('Invokes the onLogin() callback when submit button is pressed', () => {
+        const mockLogin = jest.fn()
+        const expectedLoginCredentials: LoginCredentials = 
+        {
+            username: 'Nove', 
+            password: 'abc123'
+        }
+
+        render(<LoginForm onLogin={mockLogin}/>)
+
+        const usernameInput = screen.getByLabelText(/username/i)
+        const passwordInput = screen.getByLabelText(/password/i)
+
+        fireEvent.change(usernameInput, {target: {value: expectedLoginCredentials.username}})
+        fireEvent.change(passwordInput, {target: {value: expectedLoginCredentials.password}})
+
+        const button = screen.getByRole('button')
+        fireEvent.click(button)
+
+        expect(mockLogin).toHaveBeenCalledTimes(1)
+        expect(mockLogin).toHaveBeenCalledWith(expectedLoginCredentials)
+    })
+})
