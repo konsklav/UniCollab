a3447ebd7653d29ece931c47541342e189a29393
George Nikolaidis <cptgiorgos03@gmail.com>
2025-01-15 14:08:31 +0200

    Implement GetUserUseCase

---
+++ apps/api/Saas.Api/Saas.Application/UseCases/GetUserUseCase.cs
@@ -0,0 +1,17 @@
+﻿using Ardalis.Result;
+using Saas.Application.Interfaces;
+using Saas.Domain;
+
+namespace Saas.Application.UseCases;
+
+public class GetUserUseCase(IUserRepository repository)
+{
+    public async Task<Result<User>> Handle(Guid userId)
+    {
+        var user = await repository.GetUserByIdAsync(userId);
+        if (user is null) 
+            return Result<User>.NotFound();
+
+        return user;
+    }
+}
