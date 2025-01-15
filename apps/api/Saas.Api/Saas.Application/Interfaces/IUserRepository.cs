3b7f3b7a83fd81fd0575b3b9ec2ea310d853a875
George Nikolaidis <cptgiorgos03@gmail.com>
2025-01-15 14:08:14 +0200

    Remove unnecessary method in IUserRepository.cs

--- apps/api/Saas.Api/Saas.Application/Interfaces/IUserRepository.cs
+++ apps/api/Saas.Api/Saas.Application/Interfaces/IUserRepository.cs
@@ -5,5 +5,4 @@ namespace Saas.Application.Interfaces;
 public interface IUserRepository : IUnitOfWork
 {
     Task<User?> GetUserByIdAsync(Guid id);
-    Task<List<User>> GetFriendsAsync(Guid userId);
 }
