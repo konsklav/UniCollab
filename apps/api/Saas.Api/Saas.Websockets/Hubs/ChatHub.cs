5856e4bf9422288411bb6023fa75b09e220bcd9a
George Nikolaidis <cptgiorgos03@gmail.com>
2025-01-15 19:10:40 +0200

    Add SignalR hubs

---
+++ apps/api/Saas.Api/Saas.Websockets/Hubs/ChatHub.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.RegularExpressions;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Saas.Websockets.Hubs;
+
+internal interface IChatClient
+{
+    Task ReceiveMessage(string username, string message);
+}
+
+internal sealed class ChatHub(IHubContext<NotificationHub, INotificationClient> notificationHub) : Hub<IChatClient>
+{
+    public async Task JoinChat(Guid chatId)
+    {
+        await Groups.AddToGroupAsync(Context.ConnectionId, chatId.ToString());
+    }
+
+    public async Task LeaveChat(Guid chatId)
+    {
+        await Groups.RemoveFromGroupAsync(Context.ConnectionId, chatId.ToString());
+    }
+    
+    public async Task SendMessage(Guid chatId, string username, string message)
+    {
+        await Clients.Group(chatId.ToString()).ReceiveMessage(username, message);
+        notificationHub.Clients.GroupExcept(chatId.ToString(), Context.ConnectionId);
+    }
+}
---
+++ apps/api/Saas.Api/Saas.Websockets/Hubs/NotificationHub.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Saas.Websockets.Models.Notifications;
+
+namespace Saas.Websockets.Hubs;
+
+internal sealed class NotificationHub : Hub<INotificationClient>;
+
+internal interface INotificationClient
+{
+    Task GetNotification(NotificationDto notification);
+}
