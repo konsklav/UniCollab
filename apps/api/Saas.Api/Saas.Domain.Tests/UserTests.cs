4f984dbb547a669927fcade9429c83a2ae8c245e
Konstantinos Sklavenitis <konstantinos.sklaveniths@gmail.com>
2025-01-16 21:11:13 +0200

    Addded tests for JoinChat and LeaveChat behaviours.

--- apps/api/Saas.Api/Saas.Domain.Tests/UserTests.cs
+++ apps/api/Saas.Api/Saas.Domain.Tests/UserTests.cs
@@ -117,4 +117,43 @@ public class UserTests
         
         result.IsSuccess.Should().BeTrue();
     }
+
+    [Fact]
+    public void JoinChat_Should_SuccessfullyAddUserToChat()
+    {
+        // Arrange
+        var user1 = FakeUsers.Generate();
+        var user2 = FakeUsers.Generate();
+        var chatroom = new ChatRoom("test", "test", [user1], []);
+        
+        // Act
+        var result = user2.JoinChat(chatroom);
+        
+        // Assert
+        chatroom.Participants.Should().Contain(user2);
+        
+        result.IsSuccess.Should().BeTrue();
+    }
+
+    [Fact]
+    public void SendMessage_Should_SuccessfullyAddMessageToChat()
+    {
+        // To be implemented tomorrow!
+    }
+    
+    [Fact]
+    public void LeaveChat_Should_SuccessfullyRemoveUserFromChat()
+    {
+        // Arrange
+        var user = FakeUsers.Generate();
+        var chatroom = new ChatRoom("test", "test", [user], []);
+        
+        // Act
+        var result = user.LeaveChat(chatroom);
+        
+        // Assert
+        chatroom.Participants.Should().NotContain(user);
+        
+        result.IsSuccess.Should().BeTrue();
+    }
 }
