174e5196f72b382c1b398228feccbd8c76f7af94
George Nikolaidis <cptgiorgos03@gmail.com>
2025-01-17 20:26:57 +0200

    Added automatic migrations + fake users on API startup

--- apps/api/Saas.Api/Saas.Infrastructure/DependencyInjection.cs
+++ apps/api/Saas.Api/Saas.Infrastructure/DependencyInjection.cs
@@ -1,17 +1,20 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 using Saas.Application.Interfaces;
 using Saas.Application.Interfaces.Data;
 using Saas.Infrastructure.Events;
 using Saas.Infrastructure.Repositories;
+using Saas.Tests.Fakes;
 
 namespace Saas.Infrastructure;
 
 public static class DependencyInjection
 {
-    public static void AddInfrastructure(
+    public static async Task AddInfrastructure(
         this IServiceCollection services,
         IConfiguration configuration)
     {
@@ -22,6 +25,26 @@ public static class DependencyInjection
             options.LogTo(Console.WriteLine, LogLevel.Information);
         });
 
+        using var scope = services.BuildServiceProvider().CreateScope();
+        var dbContext = scope.ServiceProvider.GetRequiredService<UniCollabContext>();
+
+        var connected = false;
+        while (!connected)
+        {
+            try
+            {
+                await dbContext.Database.MigrateAsync();
+                connected = true;
+            }
+            catch (SqlException)
+            {
+                await Task.Delay(400);
+            }
+        }
+        
+        dbContext.Users.AddRange(FakeUsers.Generate(5));
+        await dbContext.SaveChangesAsync();
+        
         services.AddScoped<IEventService, EventService>();
     }
 }
