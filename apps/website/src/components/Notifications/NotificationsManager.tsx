98d8f1b48ff96fdd9de73ef961b0ef9bafe8562a
George Nikolaidis <cptgiorgos03@gmail.com>
2025-01-21 16:08:40 +0200

    Ensure notification manager only connects to SignalR when user is authenticated

--- apps/website/src/components/Notifications/NotificationsManager.tsx
+++ apps/website/src/components/Notifications/NotificationsManager.tsx
@@ -4,11 +4,20 @@ import { useNotificationStore } from "../../state/notifications/notificationStor
 import { NotificationContract } from "./Notifications.types";
 import NotificationDisplay from "./Notification";
 import { ToastType } from "../Toasts/Toast.types";
+import { useAuth } from "../../state/authentication/authenticationStore";
 
 export default function NotificationsManager() {
+    const { user, isAuthenticated } = useAuth()
     const publish = useNotificationStore(state => state.publish)
 
     useEffect(() => {
+        if (!isAuthenticated())
+        {
+            console.log('Aborting SignalR connection attempt. Not authenticated.')
+            return;
+        }
+
+        console.log('Beginning SignalR connection.')
         const signalR = new SignalRService('notifications')
         
         signalR.startConnection()
@@ -19,7 +28,7 @@ export default function NotificationsManager() {
         })
         
         return () => signalR.stopConnection()
-    }, [])
+    }, [user])
 
     return <></>
 }
