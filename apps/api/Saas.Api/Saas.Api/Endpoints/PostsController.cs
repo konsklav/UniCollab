3d1999bad3be2bf8e63e545ab4752ae6c68cdd32
George Nikolaidis <cptgiorgos03@gmail.com>
2025-01-21 09:22:27 +0200

    Implemented /posts/{slug} endpoint

--- apps/api/Saas.Api/Saas.Api/Endpoints/PostsController.cs
+++ apps/api/Saas.Api/Saas.Api/Endpoints/PostsController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Ardalis.Result.AspNetCore;
+using Microsoft.AspNetCore.Mvc;
+using Saas.Api.Contracts;
+using Saas.Application.UseCases.Posts;
 
 namespace Saas.Api.Endpoints;
 
@@ -13,9 +16,15 @@ public class PostsController
     /// Retrieve a post by its slug. Slugs are unique so it is guaranteed that you will get back a single post.  
     /// </summary>
     /// <param name="slug">The slug to search for.</param>
+    /// <param name="getPost"></param>
     [HttpGet("{slug:alpha}")]
-    public async Task<IResult> GetPostBySlug(string slug)
+    public async Task<IResult> GetPostBySlug(string slug, [FromServices] GetPost getPost)
     {
-        throw new NotImplementedException();
+        var result = await getPost.BySlug(slug);
+        if (!result.IsSuccess)
+            return result.ToMinimalApiResult();
+
+        var post = result.Value;
+        return Results.Ok(PostDto.From(post));
     }
 }
