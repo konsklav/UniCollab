83e49926b728e5d1139793fbc09cc220fe947f6d
Konstantinos Sklavenitis <konstantinos.sklaveniths@gmail.com>
2025-01-21 14:45:58 +0200

    Implemented basic authentication

---
+++ apps/api/Saas.Api/Saas.Api/Authentication/BasicAuthenticationOptions.cs
@@ -0,0 +1,7 @@
+using Microsoft.AspNetCore.Authentication;
+
+namespace Saas.Api.Authentication;
+
+public class BasicAuthenticationOptions : AuthenticationSchemeOptions
+{
+}
--- apps/api/Saas.Api/Saas.Api/Program.cs
+++ apps/api/Saas.Api/Saas.Api/Program.cs
@@ -1,4 +1,5 @@
 using System.Reflection;
+using Saas.Api.Authentication;
 using Saas.Api.Configuration;
 using Saas.Application;
 using Saas.Infrastructure;
@@ -10,6 +11,10 @@ builder.Configuration.AddEnvironmentVariables("UNICOLLAB_");
 
 builder.Services.AddControllers();
 builder.Services.ConfigureCors(builder.Configuration);
+
+builder.Services.AddAuthentication("Basic")
+    .AddScheme<BasicAuthenticationOptions, BasicAuthenticationHandler>("Basic", null);
+
 builder.Services.AddSwaggerGen(options =>
 {
     var xml = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
@@ -24,7 +29,7 @@ var app = builder.Build();
 
 app.UseSwagger();
 app.UseConfiguredCors();
-
+app.UseAuthorization();
 app.MapControllers();
 app.MapHubs();
 app.Run();
