28f2e117a9f13c155e2a7e3c2e3ce984b59594b3
George Nikolaidis <cptgiorgos03@gmail.com>
2025-01-21 08:11:02 +0200

    Add GetPostBySlug method in PostsController

---
+++ apps/api/Saas.Api/Saas.Api/Endpoints/PostsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Saas.Api.Endpoints;
+
+/// <summary>
+/// Methods for interacting with/modifying posts
+/// </summary>
+[ApiController]
+[Route("/posts")]
+public class PostsController
+{
+    /// <summary>
+    /// Retrieve a post by its slug. Slugs are unique so it is guaranteed that you will get back a single post.  
+    /// </summary>
+    /// <param name="slug">The slug to search for.</param>
+    [HttpGet("{slug:alpha}")]
+    public async Task<IResult> GetPostBySlug(string slug)
+    {
+        throw new NotImplementedException();
+    }
+}
