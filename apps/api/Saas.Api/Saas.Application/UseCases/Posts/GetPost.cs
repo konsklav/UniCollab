d9060aa700b5c9aa511ca8b8a0ca25cc3594dd83
George Nikolaidis <cptgiorgos03@gmail.com>
2025-01-21 09:21:18 +0200

    Created GetPost use case

---
+++ apps/api/Saas.Api/Saas.Application/UseCases/Posts/GetPost.cs
@@ -0,0 +1,17 @@
+﻿using Ardalis.Result;
+using Saas.Application.Interfaces.Data;
+using Saas.Domain.Posts;
+
+namespace Saas.Application.UseCases.Posts;
+
+public sealed class GetPost(IPostRepository repository)
+{
+    public async Task<Result<Post>> BySlug(string slug)
+    {
+        var post = await repository.GetBySlugAsync(slug);
+        if (post is null)
+            return Result.NotFound($"Couldn't find post with slug '{slug}'");
+
+        return post;
+    }
+} 
