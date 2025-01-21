e1cb5789f6df2381967ebb48ee2b99a87f51df78
George Nikolaidis <cptgiorgos03@gmail.com>
2025-01-21 09:20:24 +0200

    Implement IPostRepository

---
+++ apps/api/Saas.Api/Saas.Infrastructure/Repositories/PostRepository.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Saas.Application.Interfaces.Data;
+using Saas.Domain.Posts;
+
+namespace Saas.Infrastructure.Repositories;
+
+internal sealed class PostRepository(UniCollabContext context) : IPostRepository
+{
+    public async Task<Post?> GetBySlugAsync(string slug)
+    {
+        var post = await context.Posts.FirstOrDefaultAsync(p => p.Slug == slug);
+        return post;
+    }
+    
+    public async Task SaveChangesAsync() => await context.SaveChangesAsync();
+}
