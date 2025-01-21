62ce77ccf455d8da33db09f14257aebd023883f3
George Nikolaidis <cptgiorgos03@gmail.com>
2025-01-21 08:36:22 +0200

    Added SlugHelper (with tests) to assist in slug generation.

---
+++ apps/api/Saas.Api/Saas.Domain.Tests/Common/SlugHelperTests.cs
@@ -0,0 +1,19 @@
+﻿using Saas.Domain.Common;
+
+namespace Saas.Domain.Tests.Common;
+
+public class SlugHelperTests
+{
+    [Theory]
+    [InlineData("Hello There!", "hello-there")]
+    [InlineData("Numb3rs 4r3 n0t Ignored!@#", "numb3rs-4r3-n0t-ignored")]
+    [InlineData(" Leading or Trailing   whitespace IGNORED!  ", "leading-or-trailing-whitespace-ignored")]
+    public void ShouldGenerateSlugs_InKebabCase_WithoutSpecialCharacters(string title, string expected)
+    {
+        // Act
+        var result = SlugHelper.Get(title);
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
+}
