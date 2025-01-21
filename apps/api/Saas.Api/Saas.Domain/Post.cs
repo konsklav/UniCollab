b703fc81387508b40cccdb25929e082abc67cb0b
George Nikolaidis <cptgiorgos03@gmail.com>
2025-01-21 08:35:55 +0200

    Created factory method for Post. Added a slug property

--- apps/api/Saas.Api/Saas.Domain/Post.cs
+++ apps/api/Saas.Api/Saas.Domain/Post.cs
@@ -1,3 +1,4 @@
+using Ardalis.Result;
 using Saas.Domain.Common;
 
 namespace Saas.Domain;
@@ -5,16 +6,23 @@ namespace Saas.Domain;
 public class Post : Entity
 {
     private Post() {}
-    public Post(string title, string content, List<Subject> subjects, User author, Guid? id = null) : base(id)
+    private Post(string title, string content, List<Subject> subjects, User author, string slug, Guid? id = null) : base(id)
     {
         Title = title;
         Content = content;
         Subjects = subjects;
         Author = author;
+        Slug = slug;
     }
 
     public string Title { get; private set; }
     public string Content { get; private set; }
+    public string Slug { get; private set; }
     public IReadOnlyList<Subject> Subjects { get; private set; }
     public User Author { get; private set; }
+
+    public static Result<Post> Create(string title, string content, List<Subject> subjects, User author)
+    {
+        return new Post(title, content, subjects, author, SlugHelper.Get(title));
+    }
 }
