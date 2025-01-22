ef4f6a6c6abdc576e90e7c6acb841513cadd3636
Konstantinos Sklavenitis <konstantinos.sklaveniths@gmail.com>
2025-01-22 15:45:03 +0200

    Implemented chatrooms endpoint logic for GetAllChatrooms and GetChatRoom by Id use cases (not functional yet due to dbcontext issues)

---
+++ apps/api/Saas.Api/Saas.Api/Endpoints/ChatRoomController.cs
@@ -0,0 +1,43 @@
+using Ardalis.Result.AspNetCore;
+using Microsoft.AspNetCore.Mvc;
+using Saas.Api.Contracts;
+using Saas.Application.UseCases;
+
+namespace Saas.Api.Endpoints;
+
+[ApiController]
+[Route("/chatrooms")]
+public class ChatRoomController : ControllerBase
+{
+    /// <summary>
+    /// Retrieve all the chatrooms in the system.
+    /// </summary>
+    /// <returns>A list of chatrooms</returns>
+    [HttpGet]
+    public async Task<IResult> GetAll([FromServices] GetAllChatRoomsUseCase getAllChatRooms)
+    {
+        var result = await getAllChatRooms.Handle();
+        if (!result.IsSuccess)
+            return result.ToMinimalApiResult();
+
+        var chatRooms = result.Value;
+        return Results.Ok(chatRooms);
+    }
+    
+    /// <summary>
+    /// Retrieve a chatroom by their ID, if found.
+    /// </summary>
+    /// <param name="chatRoomId">The ID to search.</param>
+    /// <param name="getChatRoom"></param>
+    /// <returns>A chat room</returns>
+    [HttpGet("{chatRoomId:guid}")]
+    public async Task<IResult> Get(Guid chatRoomId, [FromServices] GetChatRoomUseCase getChatRoom)
+    {
+        var result = await getChatRoom.Handle(chatRoomId);
+        if (!result.IsSuccess)
+            return result.ToMinimalApiResult();
+    
+        var chatRoom = result.Value;
+        return Results.Ok(chatRoom);
+    }
+}
