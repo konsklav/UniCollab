a4538eab3394a3615874a292ea1a3a8464982647
Konstantinos Sklavenitis <konstantinos.sklaveniths@gmail.com>
2025-01-22 15:36:55 +0200

    Refactored context parameter name and simplified SaveChangesAsync method in UserRepository.cs

--- apps/api/Saas.Api/Saas.Infrastructure/Repositories/UserRepository.cs
+++ apps/api/Saas.Api/Saas.Infrastructure/Repositories/UserRepository.cs
@@ -4,16 +4,16 @@ using Saas.Domain;
 
 namespace Saas.Infrastructure.Repositories;
 
-internal class UserRepository(UniCollabContext db) : IUserRepository
+internal class UserRepository(UniCollabContext context) : IUserRepository
 {
     public async Task<List<User>> GetAllAsync()
     {
-        return await db.Users.ToListAsync();
+        return await context.Users.ToListAsync();
     }
 
     public async Task<User?> GetByIdAsync(Guid userId)
     {
-        var user = await db.Users
+        var user = await context.Users
             .Include(u => u.Friends)
             .Include(u => u.Posts)
             .FirstOrDefaultAsync(u => u.Id == userId);
@@ -21,8 +21,5 @@ internal class UserRepository(UniCollabContext db) : IUserRepository
         return user;
     }
     
-    public async Task SaveChangesAsync()
-    {
-        await db.SaveChangesAsync();
-    }
+    public async Task SaveChangesAsync() => await context.SaveChangesAsync();
 }
