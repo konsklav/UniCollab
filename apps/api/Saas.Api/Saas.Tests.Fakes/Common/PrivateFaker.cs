17ae69ff1ebb005b30736d9663b6eb2038083c02
George Nikolaidis <cptgiorgos03@gmail.com>
2025-01-24 20:38:22 +0200

    Created PrivateBinder for PrivateFaker class to allow setting private fields

--- apps/api/Saas.Api/Saas.Tests.Fakes/Common/PrivateFaker.cs
+++ apps/api/Saas.Api/Saas.Tests.Fakes/Common/PrivateFaker.cs
@@ -1,10 +1,14 @@
-﻿using Bogus;
+﻿using System.Reflection;
+using Bogus;
 
 namespace Saas.Tests.Fakes.Common;
 
 // https://github.com/bchavez/Bogus/issues/213#issuecomment-475985821
 internal sealed class PrivateFaker<T> : Faker<T> where T : class
 {
+    public PrivateFaker() { }
+    public PrivateFaker(PrivateBinder binder) : base(binder: binder) { }
+    
     public PrivateFaker<T> UsePrivateConstructor() =>
         (CustomInstantiator(_ => (Activator.CreateInstance(typeof(T), nonPublic: true) as T)!) as PrivateFaker<T>)!;
 }
