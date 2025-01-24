7ec94895dfe91ae64768d6143904f605cd496c7a
Konstantinos Sklavenitis <konstantinos.sklaveniths@gmail.com>
2025-01-24 18:14:05 +0200

    Changed ChatRoom class' attributes (removed string type)

--- apps/api/Saas.Api/Saas.Domain/ChatRoom.cs
+++ apps/api/Saas.Api/Saas.Domain/ChatRoom.cs
@@ -5,13 +5,12 @@ using Saas.Domain.Common;
 
 namespace Saas.Domain;
 
-public class ChatRoom(string name, string type, List<User> participants, List<Message> messages, Guid? id = null) : Entity(id)
+public class ChatRoom(string name, List<User> participants, List<Message> messages, Guid? id = null) : Entity(id)
 {
     private readonly List<User> _participants = participants;
     private readonly List<Message> _messages = messages;
     
     public string Name { get; } = name;
-    public string Type { get; } = type;
     public IReadOnlyList<User> Participants => _participants;
     public IReadOnlyList<Message> Messages => _messages;
 
