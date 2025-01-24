6262f62d34e93722700d36779c7885332c73ce63
George Nikolaidis <cptgiorgos03@gmail.com>
2025-01-24 19:04:51 +0200

    Created chat room and message migration

--- apps/api/Saas.Api/Saas.Infrastructure/Migrations/20250124170247_AddChatRoomsAndMessages.Designer.cs
+++ apps/api/Saas.Api/Saas.Infrastructure/Migrations/20250124170247_AddChatRoomsAndMessages.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Saas.Infrastructure;
 
@@ -11,9 +12,11 @@ using Saas.Infrastructure;
 namespace Saas.Infrastructure.Migrations
 {
     [DbContext(typeof(UniCollabContext))]
-    partial class UniCollabContextModelSnapshot : ModelSnapshot
+    [Migration("20250124170247_AddChatRoomsAndMessages")]
+    partial class AddChatRoomsAndMessages
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
