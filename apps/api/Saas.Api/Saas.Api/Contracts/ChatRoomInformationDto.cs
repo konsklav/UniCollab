0396131f5290368cc60ab3f0aba60cd3ce50f464
George Nikolaidis <cptgiorgos03@gmail.com>
2025-01-27 00:42:36 +0200

    Add ChatRoomInformationDto

---
+++ apps/api/Saas.Api/Saas.Api/Contracts/ChatRoomInformationDto.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Concurrent;
+using Saas.Domain;
+
+namespace Saas.Api.Contracts;
+
+public sealed record ChatRoomInformationDto(
+    Guid Id,
+    string Name,
+    int ParticipantCount)
+{
+    internal static ChatRoomInformationDto From(ChatRoom room) =>
+        new(Id: room.Id,
+            Name: room.Name.Value,
+            ParticipantCount: room.Participants.Count);
+}
