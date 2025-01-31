110091da3a9f5ba74e34a4b6326a91a687b54f7b
George Nikolaidis <cptgiorgos03@gmail.com>
2025-01-31 20:17:58 +0200

    Implement GetByUser in chat room repository

--- apps/api/Saas.Api/src/Saas.Application/UseCases/ChatRooms/GetChatRooms.cs
+++ apps/api/Saas.Api/src/Saas.Application/UseCases/ChatRooms/GetChatRooms.cs
@@ -5,16 +5,23 @@ using Saas.Domain;
 
 namespace Saas.Application.UseCases.ChatRooms;
 
-public class GetChatRooms(IChatRoomRepository roomRepository) : IApplicationUseCase
+public class GetChatRooms(IChatRoomRepository chatRepository) : IApplicationUseCase
 {
+    /// <summary>
+    /// Gets chat rooms that user specified by <paramref name="userId"/> can join.
+    /// </summary>
     public async Task<Result<List<ChatRoom>>> Joinable(Guid userId)
     {
-        var chatRooms = await roomRepository.GetJoinableFor(userId);
+        var chatRooms = await chatRepository.GetJoinableFor(userId);
         return chatRooms;
     }
     
+    /// <summary>
+    /// Gets chat rooms that user specified by <paramref name="userId"/> is a part of.
+    /// </summary>
     public async Task<Result<List<ChatRoom>>> Participating(Guid userId)
     {
-        throw new NotImplementedException();
+        var participatingChatRooms = await chatRepository.GetByUserAsync(userId);
+        return participatingChatRooms;
     }
 }
