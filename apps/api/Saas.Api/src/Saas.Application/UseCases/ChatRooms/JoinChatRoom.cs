ac1fdb7a3112ace4dc0cc32cea764742593ddb3c
George Nikolaidis <cptgiorgos03@gmail.com>
2025-01-31 22:21:54 +0200

    Fix JoinChatRoom not saving changes

--- apps/api/Saas.Api/src/Saas.Application/UseCases/ChatRooms/JoinChatRoom.cs
+++ apps/api/Saas.Api/src/Saas.Application/UseCases/ChatRooms/JoinChatRoom.cs
@@ -19,6 +19,8 @@ public class JoinChatRoom(
             return Result.NotFound($"Chat with ID '{chatId}' not found.");
 
         var joinResult = user.JoinChat(chat);
+        
+        await chatRepository.SaveChangesAsync();
         return joinResult;
     }
 }
