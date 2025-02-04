9905c174cade369f6110b5f501607b4dd13ce09e
George Nikolaidis <cptgiorgos03@gmail.com>
2025-02-04 14:12:20 +0200

    Refactor posts controllers to be RESTful

--- apps/api/Saas.Api/src/Saas.Api/Endpoints/PostsController.cs
+++ apps/api/Saas.Api/src/Saas.Api/Endpoints/PostsController.cs
@@ -48,41 +48,4 @@ public class PostsController : ControllerBase
         var postDtos = posts.Select(PostDto.From);
         return Results.Ok(postDtos);
     }
-
-    /// <summary>
-    /// Get all posts by a specific user.
-    /// </summary>
-    /// <param name="userId">The user's ID which will be used to search for posts by them.</param>
-    /// <param name="getPosts"></param>
-    [HttpGet("user/{userId:guid}")]
-    public async Task<IResult> GetPostsByUser(
-        [FromRoute] Guid userId,
-        [FromServices] GetPosts getPosts)
-    {
-        var result = await getPosts.ByUser(userId);
-        if (!result.IsSuccess)
-            return result.ToMinimalApiResult();
-
-        var posts = result.Value;
-        return Results.Ok(posts.Select(PostDto.From));
-    }
-
-    [HttpPost("user/{userId:guid}/create")]
-    public async Task<IResult> CreatePost(
-        [FromRoute] Guid userId,
-        [FromBody] CreatePostRequest request,
-        [FromServices] CreatePost createPost)
-    {
-        var result = await createPost.Handle(
-            title: request.Title,
-            content: request.Content,
-            subjects: request.Subjects,
-            authorId: userId);
-
-        if (!result.IsSuccess)
-            return result.ToMinimalApiResult();
-
-        var post = result.Value;
-        return Results.Ok(PostDto.From(post));
-    }
 }
---
+++ apps/api/Saas.Api/src/Saas.Api/Endpoints/UserPostsController.cs
@@ -0,0 +1,49 @@
+﻿using Ardalis.Result.AspNetCore;
+using Microsoft.AspNetCore.Mvc;
+using Saas.Api.Contracts;
+using Saas.Api.Contracts.Requests;
+using Saas.Application.UseCases.Posts;
+
+namespace Saas.Api.Endpoints;
+
+[ApiController]
+[Route("/users/{userId:guid}/posts")]
+public class UserPostsController : ControllerBase
+{
+    /// <summary>
+    /// Get all posts by a specific user.
+    /// </summary>
+    /// <param name="userId">The user's ID which will be used to search for posts by them.</param>
+    /// <param name="getPosts"></param>
+    [HttpGet(Name = "Get User's Posts")]
+    public async Task<IResult> GetPostsByUser(
+        [FromRoute] Guid userId,
+        [FromServices] GetPosts getPosts)
+    {
+        var result = await getPosts.ByUser(userId);
+        if (!result.IsSuccess)
+            return result.ToMinimalApiResult();
+
+        var posts = result.Value;
+        return Results.Ok(posts.Select(PostDto.From));
+    }
+    
+    [HttpPost(Name = "Create Post")]
+    public async Task<IResult> CreatePost(
+        [FromRoute] Guid userId,
+        [FromBody] CreatePostRequest request,
+        [FromServices] CreatePost createPost)
+    {
+        var result = await createPost.Handle(
+            title: request.Title,
+            content: request.Content,
+            subjects: request.Subjects,
+            authorId: userId);
+
+        if (!result.IsSuccess)
+            return result.ToMinimalApiResult();
+
+        var post = result.Value;
+        return Results.Ok(PostDto.From(post));
+    }
+}
