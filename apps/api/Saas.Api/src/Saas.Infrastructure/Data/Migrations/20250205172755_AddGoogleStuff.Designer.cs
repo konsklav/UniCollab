82371da2874f70b12c545d7523a267928857e2df
George Nikolaidis <cptgiorgos03@gmail.com>
2025-02-05 19:28:42 +0200

    Add migration for Google integration

--- apps/api/Saas.Api/src/Saas.Infrastructure/Data/Migrations/20250205172755_AddGoogleStuff.Designer.cs
+++ apps/api/Saas.Api/src/Saas.Infrastructure/Data/Migrations/20250205172755_AddGoogleStuff.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Saas.Infrastructure;
@@ -11,9 +12,11 @@ using Saas.Infrastructure;
 namespace Saas.Infrastructure.Data.Migrations
 {
     [DbContext(typeof(UniCollabContext))]
-    partial class UniCollabContextModelSnapshot : ModelSnapshot
+    [Migration("20250205172755_AddGoogleStuff")]
+    partial class AddGoogleStuff
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ apps/api/Saas.Api/src/Saas.Infrastructure/Data/Migrations/20250205172755_AddGoogleStuff.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Saas.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGoogleStuff : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "Users",
+                type: "varchar(128)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(128)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "GoogleId",
+                table: "Users",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "CreatorId",
+                table: "Groups",
+                type: "uuid",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "GoogleId",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "Users",
+                type: "varchar(128)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "varchar(128)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "CreatorId",
+                table: "Groups",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uuid",
+                oldNullable: true);
+        }
+    }
+}
