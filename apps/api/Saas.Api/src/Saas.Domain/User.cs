a187fd7d1273bfed99aad3de54af0f215c5b820c
George Nikolaidis <cptgiorgos03@gmail.com>
2025-02-05 18:12:08 +0200

    Add CreateWithGoogle domain method

--- apps/api/Saas.Api/src/Saas.Domain/User.cs
+++ apps/api/Saas.Api/src/Saas.Domain/User.cs
@@ -14,12 +14,13 @@ public class User : Entity
     public const int MaxPasswordLength = 128;
 
     private User() {} // It's never used but it's mandatory for EF-Core!
-    private User(string username, string password, List<User> friends, List<Post> posts, Guid? id = null) : base(id)
+    private User(string username, string? password, string? googleId, List<User> friends, List<Post> posts, Guid? id = null) : base(id)
     {
         _friends = friends;
         _posts = posts;
         Username = username;
         Password = password;
+        GoogleId = googleId;
     }
 
     public string Username { get; private set; }
@@ -32,20 +33,50 @@ public class User : Entity
 
     public static Result<User> Create(string username, string password)
     {
-        if (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password))
+        var usernameValidation = ValidateUsername(username);
+        if (!usernameValidation.IsSuccess)
+            return usernameValidation;
+        
+        if (string.IsNullOrWhiteSpace(password))
             return Result.Invalid(new ValidationError("Username and/or password cannot be empty."));
-
-        if (username.Length > MaxUsernameLength)
-            return Result.Invalid(new ValidationError($"Username can be no more than {MaxUsernameLength} letters."));
 
         if (password.Length > MaxPasswordLength)
             return Result.Invalid(new ValidationError($"Password can be no more than {MaxPasswordLength} letters."));
 
         return new User(
-            username,
-            password,
+            username: username,
+            password: password,
+            googleId: null,
             friends: [],
             posts: []);
+    }
+
+    public static Result<User> CreateWithGoogle(string username, string googleId)
+    {
+        var usernameValidation = ValidateUsername(username);
+        if (!usernameValidation.IsSuccess)
+            return usernameValidation;
+
+        if (string.IsNullOrWhiteSpace(googleId))
+            return Result.Invalid(new ValidationError("Google ID cannot be empty."));
+
+        return new User(
+            username: username,
+            password: null,
+            googleId: googleId,
+            friends: [],
+            posts: []);
+    }
+
+    private static Result ValidateUsername(string username)
+    {
+        if (string.IsNullOrWhiteSpace(username))
+            return Result.Invalid(new ValidationError("Username cannot be empty."));
+
+        if (username.Length > MaxUsernameLength)
+            return Result.Invalid(new ValidationError($"Username can be no more than {MaxUsernameLength} letters."));
+
+        return Result.Success();
     }
 
     public Result AddFriend(User user)
